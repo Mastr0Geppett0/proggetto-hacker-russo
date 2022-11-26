15723bb04925137d69f773ea6e9b5950f4e7d0fb
MastroGeppett0 <118362882+MastroGppett0@users.noreply.github.com>
2022-11-26 10:46:20 +0100

    Add files via upload

--- proggetto hacker russo/palle.cpp
+++ proggetto hacker russo/palle.cpp
@@ -2,9 +2,13 @@
 #include <string>
 using namespace std;
 // made by jeff rossi and pietro lavecchia
+int sommaInteri(int x, int y){
+    int somma;
+    somma = x+y;
+}
 int main(int argc, char** argv) {
 //Sezione dichiarazioni variabili e costanti
-int a,b;
+int a,b,c;
 
 //Sezione acquisizione input palle
 printf("\ninserisci il valore di a: ");
@@ -12,8 +16,8 @@ scanf("%d", &a);
 printf("\ninserisci il valore di b: ");
 scanf("%d", &b);
 //Sezione elaborazione
-
+c = sommaInteri(a,b);
 //Sezione presentazione degli input
-
+printf("La somma e:%d",c)
 return 0;
 }
