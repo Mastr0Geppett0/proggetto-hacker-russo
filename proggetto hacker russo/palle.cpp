19db42b7facd02b73b73faff2a32d117c344cd71
MastroGeppett0 <118362882+MastroGppett0@users.noreply.github.com>
2022-11-26 10:39:26 +0100

    Add files via upload

---
+++ proggetto hacker russo/palle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+// made by jeff rossi and pietro lavecchia
+int main(int argc, char** argv) {
+//Sezione dichiarazioni variabili e costanti
+int a,b;
+
+//Sezione acquisizione input palle
+printf("\ninserisci il valore di a: ");
+scanf("%d", &a);
+printf("\ninserisci il valore di b: ");
+scanf("%d", &b);
+//Sezione elaborazione
+
+//Sezione presentazione degli input
+
+return 0;
+}
